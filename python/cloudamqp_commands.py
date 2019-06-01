6a17ac4820b9847b2ae154d7d40670d818bfba27
Giã Dương Đức Minh <minh.gdd@teko.vn>
2019-06-01 21:33:05 +0700

    Rename variable

--- python/cloudamqp_commands.py
+++ python/cloudamqp_commands.py
@@ -15,12 +15,13 @@ host = os.environ.get('AMQP_HOST', 'amqp_host')
 url = 'https://{}/api/queues/'.format(host)
 response = requests.get(url, auth=(auth_data['username'], auth_data['password']))
 data = json.loads(response.content)
-vhost = os.environ.get('AMQP_HOST', 'amqp_host')
+vhost = os.environ.get('AMQP_VHOST', 'amqp_vhost')
 
 for queue in data:
+    print(json.dumps(queue))
     # if queue['vhost'] == vhost and queue['name'].find('amq.gen') == 0:
     #     print(requests.delete('{}{}/{}'.format(url, 'virtual_host', queue['name']), auth=(auth_data['username'], auth_data['password'])).content)
     # if queue['name'] == 'teko.monitor':
-    #     print(json.dumps(queue))
-    if queue['consumers'] == 0:
-        print(queue['name'] + ' | READY: ' + str(queue['messages_ready']))
+        # print(json.dumps(queue))
+    # if queue['consumers'] == 0:
+    #     print(queue['name'] + ' | READY: ' + str(queue['messages_ready']))
